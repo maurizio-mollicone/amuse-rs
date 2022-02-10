bb3ca8d8f1504b5ac897d8f0b8d28f3777754334
Maurizio Mollicone <mollik@gmail.com>
2022-02-10 02:37:52 +0100

    Allow Swagger urls

--- src/main/java/it/mollik/amuse/amusers/config/SecurityConfig.java
+++ src/main/java/it/mollik/amuse/amusers/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,49 +23,63 @@ import it.mollik.amuse.amusers.service.impl.AmuseUserDetailsService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
 		// securedEnabled = true
-		//,
+		// ,
 		// jsr250Enabled = true,
-		prePostEnabled = true
-)
-public class SecurityConfig extends WebSecurityConfigurerAdapter  {
-    
-    @Autowired
-	AmuseUserDetailsService amuseUserDetailsService;
+		prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String[] AUTH_WHITELIST = {
+			// -- Swagger UI v2
+			"/v2/api-docs",
+			"/swagger-resources",
+			"/swagger-resources/**",
+			"/configuration/ui",
+			"/configuration/security",
+			"/swagger-ui.html",
+			"/webjars/**",
+			// -- Swagger UI v3 (OpenAPI)
+			"/v3/api-docs/**",
+			"/swagger-ui/**"
+			// other public endpoints of your API may be appended to this array
+	};
 	
-    @Autowired
+	@Autowired
+	AmuseUserDetailsService amuseUserDetailsService;
+
+	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
 
 	@Bean
 	public AuthTokenFilter authenticationJwtTokenFilter() {
 		return new AuthTokenFilter();
 	}
-	
-    @Override
+
+	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 		authenticationManagerBuilder.userDetailsService(amuseUserDetailsService).passwordEncoder(passwordEncoder());
 	}
-	
-    @Bean
+
+	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
-    @Bean
+
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-    @Override
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
-		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-		.authorizeRequests().antMatchers("/**").permitAll().and()
-		.authorizeRequests().antMatchers("/v3/api-docs/**").permitAll().and()
-		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-		.antMatchers("/api/test/**").permitAll()
-		.anyRequest().authenticated();
+				.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+				.antMatchers(AUTH_WHITELIST).permitAll()
+				.antMatchers("/api/test/**").permitAll()
+				.anyRequest().authenticated();
+
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
-}   
+}
