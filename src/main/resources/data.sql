e4e5ff28c03f3f02964f7b69456b2a9bf2b78806
Maurizio Mollicone <mollik@gmail.com>
2022-02-12 04:25:39 +0100

    Fix script for MariaDB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,16 +14,16 @@ INSERT INTO user (create_ts,name,status,update_ts,email,password,user_name,user_
 	 ('2022-02-10 09:08:32.581','user04','INSERT','2022-02-10 09:08:32.581','user04@localhost','$2a$10$5vw2bNRLaWP6lurWpqjb8uEmMw8wnf5mxI0B3nkGQarJ9Oizl52XO','user04',0);
 
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','admin','ADMIN',SELECT id from USER u WHERE u.user_name='admin');
+	 ('2022-02-10 09:08:32.581','admin','ADMIN', (SELECT id from user u WHERE u.user_name='admin'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','manager01','MANAGER',SELECT id from USER u WHERE u.user_name='manager01');
+	 ('2022-02-10 09:08:32.581','manager01','MANAGER',(SELECT id from user u WHERE u.user_name='manager01'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','manager02','MANAGER',SELECT id from USER u WHERE u.user_name='manager02');
+	 ('2022-02-10 09:08:32.581','manager02','MANAGER',(SELECT id from user u WHERE u.user_name='manager02'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','user01','USER',SELECT id from USER u WHERE u.user_name='user01');
+	 ('2022-02-10 09:08:32.581','user01','USER',(SELECT id from user u WHERE u.user_name='user01'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','user02','USER',SELECT id from USER u WHERE u.user_name='user02');
+	 ('2022-02-10 09:08:32.581','user02','USER',(SELECT id from user u WHERE u.user_name='user02'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','user03','USER',SELECT id from USER u WHERE u.user_name='user03');
+	 ('2022-02-10 09:08:32.581','user03','USER',(SELECT id from user u WHERE u.user_name='user03'));
 INSERT INTO role (create_ts,user_name,user_role,user_id) VALUES
-	 ('2022-02-10 09:08:32.581','user04','USER',SELECT id from USER u WHERE u.user_name='user04');
+	 ('2022-02-10 09:08:32.581','user04','USER',(SELECT id from user u WHERE u.user_name='user04'));
